703361f8827ba1a392bd2553339257b80ab63591
Nicolas Marchese <nicktmarchese@gmail.com>
2025-01-13 10:30:32 -0500

    created SwordWarrior class + constructors

---
+++ SwordWarrior.java
@@ -0,0 +1,17 @@
+public class SwordWarrior extends Adventurer{
+  int energy, energyMax;
+
+  public SwordWarrior(){
+    this("Carl");
+  }
+
+  public SwordWarrior(String name){
+    super(name, 30); // 30 hp
+    energy = 3;
+  }
+
+  
+
+
+
+}
