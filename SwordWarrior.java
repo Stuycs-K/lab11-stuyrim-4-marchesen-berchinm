864bf1f65d14934d3e63d33048c9d3da442e17a6
Nicolas Marchese <nicktmarchese@gmail.com>
2025-01-13 10:39:10 -0500

    Declared all abstract methods of Adventurer

--- SwordWarrior.java
+++ SwordWarrior.java
@@ -1,5 +1,5 @@
 public class SwordWarrior extends Adventurer{
-  int energy, energyMax;
+  int energy, maxEnergy;
 
   public SwordWarrior(){
     this("Carl");
@@ -7,10 +7,42 @@ public class SwordWarrior extends Adventurer{
 
   public SwordWarrior(String name){
     super(name, 30); // 30 hp
-    energy = 3;
+    energy = 3; // starting energy 3
+    energyMax = 7; // max energy 7
   }
 
-  
+  public String getSpecialName(){
+    return "energy";
+  }
+
+  public int getSpecial(){
+    return energy;
+  }
+
+  public void setSpecial(int n){
+    energy = n;
+  }
+
+  public int getSpecialMax(){
+    return maxEnergy;
+  }
+
+  public String attack(Adventurer other){
+
+  }
+
+  public String specialAttack(Adventurer other){
+
+  }
+
+  public String support(){
+
+  }
+
+  public String support(Adventurer other){
+    
+  }
+
 
 
 
