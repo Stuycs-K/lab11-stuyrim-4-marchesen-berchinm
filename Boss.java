e1cb4361979f93d1523685fef99ef1d61a5a925b
Nicolas Marchese <nicktmarchese@gmail.com>
2025-01-17 08:07:21 -0500

    finished Boss class

--- Boss.java
+++ Boss.java
@@ -8,7 +8,7 @@ public class Boss extends Adventurer{
   public Boss(String name){
     super(name, 100); // 100 hp
     power = 0; // starting power 0
-    maxPower = 100; // max power 100
+    maxPower = 10; // max power 10
   }
 
   public String getSpecialName(){
@@ -27,13 +27,40 @@ public class Boss extends Adventurer{
     return maxPower;
   }
 
-  // shoots lightning. deals damage that is 0-25% of opponent's health, but does at least 3 damage
+  // shoots lightning. deals damage that is 0-40% of opponent's health for at least 3 damage
   public String attack(Adventurer other){
-    double percentDamage = 25 * Math.random();
+    double percentDamage = 40 * Math.random();
     int damage = (int)(other.getHP() * percentDamage / 100);
+    if (damage < 3){damage = 3;}
 
     other.applyDamage(damage);
 
     return (getName() + " shot lightning and hit " + other.getName() + " for " + damage + " damage!");
   }
+
+  // shoots a massive lightning bolt. takes half of opponent's health for at least 5 damage
+  public String specialAttack(Adventurer other){
+    if (power < 10){return (getName() + " doesn't have enough energy for a special attack!");}
+    else{power -= 10;}
+
+    int damage = (int)(other.getHP() * 50 / 100);
+
+    other.applyDamage(damage);
+
+    return (getName() + " struck " + other.getName() + " with a huge lightning bolt and dealt " + damage + " damage!");
+  }
+
+  // takes a nap. gains 1-10 health and energy
+  public String support(){
+    int heal = (int)(1 + 10 * Math.random());
+    int power = (int)(1 + 10 * Math.random());
+    applyHeal(heal);
+    restoreSpecial(power);
+    return (getName() + " took a nap and gained " + heal + " health and " + power + " energy!");
+  }
+
+  public String support(Adventurer other){
+    return support();
+  }
+
 }
